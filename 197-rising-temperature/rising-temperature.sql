01543fca27d05f58473ed01bf70980f891a86f03
Lithikhaa Kathiresan <94103825+Lithikhaa@users.noreply.github.com>
2025-03-26 22:43:58 +0530

    Time: 394 ms (98.00%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,15 @@
-# Write your MySQL query statement below
-select w1.id from Weather w1
-join Weather w2 
-where DATEDIFF(w1.recordDate, w2.recordDate) = 1
-and w2.temperature < w1.temperature
+-- Create a CTE to calculate the previous day's temperature and date
+WITH q1 AS (
+  SELECT 
+      *,
+      LAG(temperature) OVER (ORDER BY recordDate) AS previous_day_temperature,
+      LAG(recordDate) OVER (ORDER BY recordDate) AS previous_Date
+  FROM Weather
+)
+
+-- Select the IDs where the temperature is higher than the previous day
+-- and the previous day exists (difference is exactly 1 day)
+SELECT id
+FROM q1
+WHERE temperature > previous_day_temperature
+AND DATEDIFF(recordDate, previous_Date) = 1;
