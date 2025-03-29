d380d74eb19d719d513695465651992b40281d4a
Lithikhaa Kathiresan <94103825+Lithikhaa@users.noreply.github.com>
2025-03-29 20:11:48 +0530

    Time: 374 ms (88.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT 
+    id,
+    CASE
+        WHEN id % 2 = 0 THEN LAG(student) OVER(ORDER BY id)
+        ELSE COALESCE(LEAD(student) OVER(ORDER BY id), student)
+    END AS student
+FROM Seat
