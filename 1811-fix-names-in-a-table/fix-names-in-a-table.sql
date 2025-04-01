716ea907f6fc7335ae9b576305986348f03cb877
Lithikhaa Kathiresan <94103825+Lithikhaa@users.noreply.github.com>
2025-04-01 23:14:20 +0530

    Time: 667 ms (87.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT user_id,CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2,length(name)))) AS name
+FROM Users ORDER BY user_id;
+# SUBSTR(string_name , start_index ,end_index)
